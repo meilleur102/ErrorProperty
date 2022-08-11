71dd7e082fd2020b1ad22d36d18c75e1e4ddec8b
meilleur102 <jean.christophe.meilleur@gmail.com>
2022-08-11 08:55:35 -0400

    Initial commit

---
+++ ErrorProperty/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ErrorProperty;
+
+
+[INotifyPropertyChanged]
+public partial class MainViewModel
+{
+    public MainViewModel()
+    {
+        Test = "test string";
+    }
+
+    [ObservableProperty] private string _test;
+}
